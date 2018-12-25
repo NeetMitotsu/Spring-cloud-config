979e2903e7b921c326ab3c0b8580c1b077f9e8eb
Company Key <lxd3808@163.com>
2018-12-25 15:12:12 +0800

    修改config-client-test.properties位置

---
+++ config-client/src/main/java/com/lxd/configclient/ConfigClientApplication.java
@@ -0,0 +1,26 @@
+package com.lxd.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value(value = "${foo}")
+    String foo;
+
+    @RequestMapping(value = "/getFoo")
+    public String foo(){
+        return foo;
+    }
+
+}
+
